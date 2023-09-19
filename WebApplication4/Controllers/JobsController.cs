e595f5979d40ff54444306815eb581045942127b
Hatice Bülbül <hatice.bulbul@ogr.sakarya.edu.tr>
2023-09-19 10:24:52 +0300

    Jobs veritabanına bağlanıldı ve Jobs Get API oluşturuldu.

---
+++ WebApplication4/Controllers/JobsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+ 
+    public class JobsController : ControllerBase
+    {
+      
+      
+        private readonly IConfiguration _configuration;
+
+        public JobsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("Jobs")]
+
+        public IEnumerable<Jobs> GetJobs()
+        {
+
+            string dbConnectionString = _configuration.GetConnectionString("JobsDbConnection");
+            SqlConnection con = new SqlConnection(dbConnectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TblJobs", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Jobs> jobs = new List<Jobs>();
+            while (reader.Read())
+            {
+                Jobs job = new Jobs();
+                job.ID = (int)reader["ID"];
+                job.name = reader["name"].ToString();
+              
+
+                jobs.Add(job);
+            }
+            con.Close();
+            return jobs;
+        }
+
+    }
+}
---
+++ WebApplication4/Models/Jobs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Jobs
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+    }
+}
